1f940b7f5520d102b5db2effa0fb3cd9608528ad
Jessica <jessicaperezvalle@pursuit.org>
2019-05-14 01:43:21 -0400

    renamed login and main activities to MainHost and Login

--- app/src/main/java/org/pursuit/stir/MainHostActivity.java
+++ app/src/main/java/org/pursuit/stir/MainHostActivity.java
@@ -3,11 +3,11 @@ package org.pursuit.stir;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainHostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_host);
     }
 }
