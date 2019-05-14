af8d342ee68ed2ffc9f1854fa407e76b3755d1c5
Jessica <jessicaperezvalle@pursuit.org>
2019-05-14 01:38:49 -0400

    created fragments and map, models and network packages

---
+++ app/src/main/java/org/pursuit/stir/SignUpFragment.java
@@ -0,0 +1,30 @@
+package org.pursuit.stir;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.pursuit.stir.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SignUpFragment extends Fragment {
+
+
+    public SignUpFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+
+}
