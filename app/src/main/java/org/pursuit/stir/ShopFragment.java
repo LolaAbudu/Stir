8ce03fe71bb763ae6745fadee99e97b54868b3d5
evelynlucas <evelynlucas@pursuit.org>
2019-05-16 20:56:40 -0400

    Added googleApiClient Builder to ShopFragment

--- app/src/main/java/org/pursuit/stir/ShopFragment.java
+++ app/src/main/java/org/pursuit/stir/ShopFragment.java
@@ -1,16 +1,22 @@
 package org.pursuit.stir;
 
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 
 /**
@@ -20,11 +26,15 @@ public class ShopFragment extends Fragment
         implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleApiClient googleApiClient;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+
+    private String foursquareClientID;
+    private String foursquareClientSecret;
 
     public ShopFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,9 +44,29 @@ public class ShopFragment extends Fragment
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.shop_recyclerview);
+        recyclerView.setHasFixedSize(true);
+        linearLayoutManager = new GridLayoutManager(view.getContext(), 2);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        googleApiClient = new GoogleApiClient.Builder(view.getContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
     public void onConnected(@Nullable Bundle bundle) {
 
-    }
+        if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        // TODO:
+
+        }
+
+        }
 
     @Override
     public void onConnectionSuspended(int i) {
