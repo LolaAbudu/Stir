80bee74edd49e605252a5c49ef7649f9edc22a50
evelynlucas <evelynlucas@pursuit.org>
2019-05-16 20:40:27 -0400

    Implemented GoogleApiClient methods in ShopFragment

--- app/src/main/java/org/pursuit/stir/ShopFragment.java
+++ app/src/main/java/org/pursuit/stir/ShopFragment.java
@@ -2,17 +2,24 @@ package org.pursuit.stir;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ShopFragment extends Fragment {
+public class ShopFragment extends Fragment
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    private GoogleApiClient googleApiClient;
 
     public ShopFragment() {
         // Required empty public constructor
@@ -26,4 +33,18 @@ public class ShopFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_shop, container, false);
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
