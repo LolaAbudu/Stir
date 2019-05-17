d1cf0fd54571034f75218c98c230c21261e5f266
LolaAbudu <lolaabudu1589@gmail.com>
2019-05-17 00:30:33 -0400

    added HomeListener Interface and replaced MainHostACtivity with HomeFragment

---
+++ app/src/main/java/org/pursuit/stir/HomeListener.java
@@ -0,0 +1,6 @@
+package org.pursuit.stir;
+
+public interface HomeListener {
+
+    void replaceWithHomeFragment();
+}
--- app/src/main/java/org/pursuit/stir/MainHostActivity.java
+++ app/src/main/java/org/pursuit/stir/MainHostActivity.java
@@ -3,7 +3,7 @@ package org.pursuit.stir;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainHostActivity extends AppCompatActivity {
+public class MainHostActivity extends AppCompatActivity implements HomeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +13,12 @@ public class MainHostActivity extends AppCompatActivity {
         replaceWithHomeFragment();
     }
 
-    private void replaceWithHomeFragment() {
+    @Override
+    public void replaceWithHomeFragment() {
         getSupportFragmentManager()
                 .beginTransaction()
                 .setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit)
-                .replace(R.id.main_host_container, ImageUploadFragment.newInstance())
+                .replace(R.id.main_host_container, HomeFragment.newInstance())
                 .commit();
     }
 }
