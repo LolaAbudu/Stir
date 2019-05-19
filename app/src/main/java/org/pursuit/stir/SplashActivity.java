c9784c9f6ee5930faae1a391fffa4bfb188120e4
Jessica <jessicaperezvalle@pursuit.org>
2019-05-19 15:57:55 -0400

    deleted toolbar in splash activity

--- app/src/main/java/org/pursuit/stir/SplashActivity.java
+++ app/src/main/java/org/pursuit/stir/SplashActivity.java
@@ -1,13 +1,36 @@
 package org.pursuit.stir;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    Handler handler;
+    private ImageView logoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.LoginTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        logoImageView = findViewById(R.id.splash_logo_imageView);
+        handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
     }
 }
