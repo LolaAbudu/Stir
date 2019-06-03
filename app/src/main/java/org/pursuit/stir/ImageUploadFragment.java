4fd547c7ac2fe1ee2d7af32ef91f4ad8c7d4f226
evelynlucas <evelynlucas@pursuit.org>
2019-06-03 19:48:32 -0400

    Imageupload frag listener

--- app/src/main/java/org/pursuit/stir/ImageUploadFragment.java
+++ app/src/main/java/org/pursuit/stir/ImageUploadFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +18,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
