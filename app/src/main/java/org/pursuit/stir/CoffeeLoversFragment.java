361aa02c1d0ae5c43536e02546cfa56be15b6f94
evelynlucas <evelynlucas@pursuit.org>
2019-06-10 19:54:47 -0400

    Implemented changes

--- app/src/main/java/org/pursuit/stir/CoffeeLoversFragment.java
+++ app/src/main/java/org/pursuit/stir/CoffeeLoversFragment.java
@@ -17,6 +17,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
